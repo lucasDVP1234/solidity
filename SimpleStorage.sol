385c55e7e7b083c61e457ea0fbf09189d10e015d
Lucas Daval-Pommier <lucasdavalpommier@gmail.com>
2023-02-02 16:19:33 +0100

    horse

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -7,7 +7,7 @@ contract SimpleStorage {
     string favoriteString = "String";
     int256 favoriteInt = -12;
     address favoriteAddress = 0x75e21f2753247574;
-    bytes32 favoriteBytes = "cat";
+    bytes32 favoriteBytes = "horse";
     bytes64 favoriteByts = "dog";
 
     
