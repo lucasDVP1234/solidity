a26fdeb7a7cdc013c0be6a94d9a7da207f2d2aae
Lucas Daval-Pommier <lucasdavalpommier@gmail.com>
2023-02-02 15:35:46 +0100

    new

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,4 +1,15 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
+
+contract SimpleStorage {
+
+    uint256 favoriteNumber = 5;
+    bool favoriteBooll = false;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favorite Address = 0x757477547574;
+    bytes32 favoriteBytes = "cat";
+    
+}
 
 
 
